7322dcdf204f973fe3a9d0af68ba38e95835643d
Galina <gmokrinskaya@gmail.com>
2019-07-19 16:32:52 +0300

    Added tests for modification of fields by name

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,13 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="New name", header="New header", footer="New footer"))
+    app.group.modify_first_group(Group(name="New name"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
