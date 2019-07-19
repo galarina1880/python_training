779729c2caa14ad871aee8e87cf7dce6629a33be
Galina <gmokrinskaya@gmail.com>
2019-07-19 12:43:42 +0300

    test for group modification edited

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 
 
-def test_add_empty_group(app):
+def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify(Group(name="name mod", header="header mod", footer="footer mod"))
     app.session.logout()
