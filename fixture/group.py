c1476d87f91b86409ca48e57f41bc8e05a4366bf
Galina <gmokrinskaya@gmail.com>
2019-07-19 15:28:49 +0300

    Defined methods for forms filling out

--- fixture/group.py
+++ fixture/group.py
@@ -7,12 +7,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def fill_group_form(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # open new group form
-        wd.find_element_by_name("new").click()
-        # fill out create group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -22,6 +18,13 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # open new group form
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
         # submit new group creation
         wd.find_element_by_name("submit").click()
         self.open_groups_page()
@@ -42,15 +45,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
         # fill out create group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit new group creation
         wd.find_element_by_name("update").click()
         self.open_groups_page()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="name mod", header="header mod", footer="footer mod"))
+    app.group.modify(Group(name="New name", header="New header", footer="New footer"))
     app.session.logout()
