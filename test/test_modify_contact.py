036c9e4fd389eaa29cde819e8a2106e30281196d
Galina <gmokrinskaya@gmail.com>
2019-07-23 14:24:01 +0300

    Adding prerequisites for modif/deleting contacts

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,5 +3,7 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify(Contact(firstname='firstname mod', middlename='middlename mod', lastname='lastname mod', nickname='nick mod', title='title mod', company='company mod', address='address mod', home='home phone mod', mobile='mobile mod', work='work phone mod', fax='fax mod', email='email 1 mod', email2='email 2 mod', email3='email 3 mod', homepage='homepage mod', bday='7', bmonth='September', byear='1988', aday='10', amonth='February', ayear='2010', address2='Address 2 mod', phone2='phone 2 mod', notes='notes mod'))
     app.session.logout()
