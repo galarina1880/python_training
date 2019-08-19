9a5d4a3fda93c86f7062f263ac87532a7de87366
Galina <gmokrinskaya@gmail.com>
2019-08-19 09:15:59 +0300

    Fixed random element selection for contact deletion/modification

--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None, phone2=None, notes=None, id=None, value=None):
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None, phone2=None, notes=None, id=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -29,13 +29,13 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
-        self.value = value
+        # self.value = value
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.value)
+        return "%s:%s" % (self.firstname, self.lastname)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and (self.value is None or other.value is None or self.value == other.value)
+        return (self.firstname is None or other.firstname is None or self.firstname == other.firstname) and (self.lastname is None or other.lastname is None or self.lastname == other.lastname)
 
     def id_or_max(self):
         if self.id:
